b22a3f43b5985fb5f84641f79f064ebeda2c17a6
JoKraken <J.kraken24@hotmail.com>
2022-09-25 14:36:18 +0200

    add list of sentence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Sentence } from './component/list/sentence';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CGMSentenceManager';
+
+  constructor() { } 
+
+  sentenceList = [{
+    n: 1,
+    s: 'Anna',
+    v: 'is eating',
+    o: 'a cookie'
+   } as Sentence,
+   {
+    n: 2,
+    s: 'Henry',
+    v: 'does',
+    o: 'a handstand'
+   } as Sentence,
+   {
+    n: 3,
+    s: 'Sarah',
+    o: 'a pen'
+   } as Sentence,
+   {
+    n: 4,
+    s: 'John',
+    v: 'tells'
+   } as Sentence,
+   {
+    n: 5,
+    s: '',
+    o: 'a drama'
+   } as Sentence];
+   
 }
---
+++ src/app/component/list/sentence.ts
@@ -0,0 +1,6 @@
+export interface Sentence {
+    n: number;
+    s: string;
+    v: string;
+    o: string;
+}
